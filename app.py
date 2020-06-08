bd77fee861f1c8f171958d246eeb8746cf04838a
Yang Yubei <yangyubei0218@gmail.com>
2020-06-08 21:21:00 +0800

    Database can show

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
@@ -8,28 +8,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weatherData.db'
 database = SQLAlchemy(app)
 
 class City(database.Model):
-    id = database.Column(database.Interval, primary_key=True)
+    id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(50), nullable=False)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    cities
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8ad2342bc9de1e81734e896a48a13c93'
-    city = 'Guangzhou'
-    response = requests.get(url.format(city)).json()
-    # print(response)
+    if request.method == 'POST':
+        new_city = request.form.get('city')
+        print(new_city)
+        if new_city:
+            new_city_obj = City(name=new_city)
+            database.session.add(new_city_obj)
+            database.session.commit()
 
-    weather_data = {
-        'City': city,
-        'Temperature': response['main']['temp'],
-        'Description': response['weather'][0]['description'],
-        'Icon': response['weather'][0]['icon'],
-        'Temperature_Max': response['main']['temp_max'],
-        'Temperature_Min': response['main']['temp_min'],
-        'Humidity': response['main']['humidity'],
-        'Pressure': response['main']['pressure'],
-        'Wind': response['wind']['speed']
-    }
-    print(weather_data)
-    return render_template('weather.html', weather_data=weather_data)
+    cities = City.query.all()
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8ad2342bc9de1e81734e896a48a13c93'
+
+    weather_data_cities = []
+    for city in cities:
+        response = requests.get(url.format(city.name)).json()
+        # print(response)
+        weather_data = {
+            'City': city.name,
+            'Temperature': response['main']['temp'],
+            'Description': response['weather'][0]['description'],
+            'Icon': response['weather'][0]['icon'],
+            'Temperature_Max': response['main']['temp_max'],
+            'Temperature_Min': response['main']['temp_min'],
+            'Humidity': response['main']['humidity'],
+            'Pressure': response['main']['pressure'],
+            'Wind': response['wind']['speed']
+        }
+        weather_data_cities.append(weather_data)
+
+    return render_template('weather.html', weather_data_cities=weather_data_cities)
